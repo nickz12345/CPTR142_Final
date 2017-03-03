436b98b2265b82263ed8a19b73d28e77b976cb24
Avion <amnatek@gmail.com>
2017-03-03 10:35:41 -0800

    Added pseudo code
    Psuedo code for program, copied some code from previous project for
    increasing array size

--- CLI_VirusSimulation/main.cpp
+++ CLI_VirusSimulation/main.cpp
@@ -15,13 +15,112 @@
 #include "Ambulance.h"
 using namespace std;
 
+// Increase array size
+int* doubleCapacity(const int* list, int size)
+{
+    int arraySize = 2 * size;
+    int* temp;
+    temp = new int[arraySize];
+    for (int i = 0; i < size; i++)
+    {
+        temp[i] = list[i];
+    }
 
-
+    return temp;
+}
 
 int main()
 {
+    // Check for user OS to see if we need to "pause" at end
+    bool windows;
+    char userOS;
+    cout << "Are you running this program on Windows? (y/n) ";
+    cin >> userOS;
+    if (userOS == 'y' || userOS == 'Y')
+    {
+        windows = true;
+    }
+    if (userOS == 'n' || userOS == 'N')
+    {
+        windows = false;
+    }
 
 
+    int numPeople;	// Array size
+    int count = 0;
+    int i;
+    int *a; // Will be array a
 
-	return 0;
+    cout << "Welcome to VirusSimulation." << endl;
+    cout << "How many people do you want in the simulation? ";
+    cin >> numPeople;
+    // Variable a = new dynamic array of user defined size
+    a = new int[numPeople];
+
+    /* psuedo code
+     * (Maybe we should use vectors to auto-resize instead of arrays)
+     * int infected;	// When this reaches the same number as the amount of people, simulation ends
+     * bool run;		// Used for while loop
+     *
+     * while (run == true)
+     * {
+     * Start timer.
+     *
+     * for (int i = 0; i < arraySize; i++)
+     * 	{
+     * 	rand();
+     * 	if (rand == true)
+     * 		{
+     * 		// Move person to infected array
+     * 		copy [i] to infected array
+     * 		infected++;
+     * 		break;	// Not sure if I'm using this correctly. Want it to go back to top of while loop.
+     * 		}
+     * 	else
+     * 		continue;
+     *
+     * 	rand();
+     * 	if (rand == true)
+     * 		{
+     * 		// Move infected to ambulance
+     * 		copy [i] to ambulance array
+     * 		break;
+     * 		}
+     * 	else
+     * 		continue;
+     *
+     * 	if (infected == numPeople)
+     * 		{
+     * 			run = false;
+     * 		}
+     * 	}
+     * }
+     *
+     * cout << "Everyone was infected" << endl;
+     * cout << "It took " << timer << "s." << endl;
+     */
+
+    // Expand array and copy contents of array a into array b
+    int *b = doubleCapacity(a, numPeople);
+    cout << "\nDebug: The 1st array's values are now in the 2nd expanded array" << endl;
+
+    // User enters data into array b
+//    for (i = numPeople; i < 2 * numPeople; i++)
+//    {
+//        cout << "\nEnter element number " << i + 1 << ": ";
+//        cin >> b[i];
+//    }
+
+    // Delete data in arrays a and b
+    delete[] a;
+    delete[] b;
+
+
+    if (windows == true)
+    {
+    system("pause");
+    return 0;
+    }
+    else
+        return 0;
 }
