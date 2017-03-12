fd555e2aee836a2cdba57a7244dbfe0265cebc8c
nickz12345 <nickteslazimmerman@gmail.com>
2017-03-12 12:26:44 -0700

    Added user input for probability
    Probability of disease at startup can be set from 0-100

--- CLI_VirusSimulation/CLI_VirusSimulation/Person.h
+++ CLI_VirusSimulation/CLI_VirusSimulation/Person.h
@@ -18,14 +18,44 @@ using namespace std;
 class Person
 {
 public:
-int health;
 int natImmunity;
+int health;
 int strain;
 
-Person(int h, int nat, int s) {
-	health = h;
-	natImmunity = nat;
-	strain = s;
+//Default constructor
+Person() {
+	natImmunity = rand() % 10;
+	
+	if(rand() % 100 <= 10)
+	{
+		health = 0;
+		strain = ((rand() % 4)+1);	//levels 1-4 are stain strengths; 0 means no strain multiplier
+	}
+	else { 
+		health = 1;
+		strain = 0;
+	     }
+}
+
+//Constructor given health probablility
+Person(double prob) {		//prob entered as 0-100; likelihood of disease at startup
+	
+	if(rand() % 101 <= prob)	//rand produces 0-100
+	{
+		health = 0;
+		strain = ((rand() % 4)+1);	//levels 1-4 are stain strengths; 0 means no strain multiplier
+	}
+	else { 
+		health = 1;
+		strain = 0;
+	     }
+}
+	
+//Full-control constructor
+Person(int nat,int h, int s) {
+	natImmunity = nat;		//0-9
+	health = h;			//0-3
+	strain = s;			//0-4
 }
 
 
